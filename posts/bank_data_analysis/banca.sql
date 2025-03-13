92294533844de519a2d6fa53da617a1099cd3d3e
SimoneBrazzi <simonebrazzi@cloud.com>
2025-03-13 11:03:51 +0100

    update .gitignore

--- posts/bank_data_analysis/banca.sql
+++ posts/bank_data_analysis/banca.sql
@@ -1,33 +1,40 @@
--- Ho capito solo ora la richiesta. Ho riletto pià volte fino a farmi entrare in testa che dovevo creare una tabella di features per un modello. Non so perché ero convinto fosse un esercizio diverso.
--- Grazie per la pazienza e spero sia la consegna definitiva :-) 
--- Di sicuro ho imparato ad interagire correttamente da CLI con un mariadb: installarlo e farlo funzionare correttamente è stata un'impresa, soprattutto perché su mac non setta correttamente la password e lavorare in locale è un'impresa.
-
-
--- crea table temporanea
-CREATE TEMPORARY TABLE IF NOT EXISTS features (
+CREATE TEMPORARY TABLE features (
   SELECT 
     cliente.id_cliente,
     -- feature età
     TIMESTAMPDIFF(YEAR, cliente.data_nascita, CURRENT_DATE()) AS eta,
     -- numero di transazioni in uscita ed entrata
-    COUNT(CASE WHEN tipo_transazione.segno = '-' THEN 1 ELSE 0 END) AS n_transazioni_uscita,
-    COUNT(CASE WHEN tipo_transazione.segno = '+' THEN 1 ELSE 0 END) AS n_transazioni_entrata,
+    COUNT(CASE WHEN tipo_transazione.segno = '-' THEN 1 END) AS n_transazioni_uscita,
+    COUNT(CASE WHEN tipo_transazione.segno = '+' THEN 1 END) AS n_transazioni_entrata,
+    -- numero di transazioni per tipologia di conto
     -- totale uscita e entrata
     SUM(CASE WHEN tipo_transazione.segno = '-' THEN transazioni.importo ELSE 0 END) AS tot_uscita,
     SUM(CASE WHEN tipo_transazione.segno = '+' THEN transazioni.importo ELSE 0 END) AS tot_entrata,
     -- numero di conti 
     COUNT(DISTINCT conto.id_conto) AS n_conti,
     -- numero di conti per tipo di conto
-    COUNT(CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Privati' THEN 1 ELSE 0 END) AS n_conto_privati,
-    COUNT(CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Base' THEN 1 ELSE 0 END) AS n_conto_base,
-    COUNT(CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Business' THEN 1 ELSE 0 END) AS n_conto_business,
-    COUNT(CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Famiglie' THEN 1 ELSE 0 END) AS n_conto_famiglie,
-    -- numero di conti per tipo di conto con operazioni in uscita
+    COUNT(DISTINCT CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Privati' THEN 1 END) AS n_conto_privati,
+    COUNT(DISTINCT CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Base' THEN 1 END) AS n_conto_base,
+    COUNT(DISTINCT CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Business' THEN 1 END) AS n_conto_business,
+    COUNT(DISTINCT CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Famiglie' THEN 1 END) AS n_conto_famiglie,
+
+    -- Numero di transazioni in uscita per tipologia di conto
+    COUNT(CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Privati' AND tipo_transazione.segno = '-' THEN tipo_transazione.segno END) AS n_uscita_conto_privati,
+    COUNT(CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Base' AND tipo_transazione.segno = '-' THEN tipo_transazione.segno END) AS n_uscita_conto_base,
+    COUNT(CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Business' AND tipo_transazione.segno = '-' THEN tipo_transazione.segno END) AS n_uscita_conto_business,
+    COUNT(CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Famiglie' AND tipo_transazione.segno = '-' THEN tipo_transazione.segno END) AS n_uscita_conto_famiglie,
+    -- Numero di transazioni in entrata per tipologia di conto
+    COUNT(CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Privati' AND tipo_transazione.segno = '+' THEN tipo_transazione.segno END) AS n_entrata_conto_privati,
+    COUNT(CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Base' AND tipo_transazione.segno = '+' THEN tipo_transazione.segno END) AS n_entrata_conto_base,
+    COUNT(CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Business' AND tipo_transazione.segno = '+' THEN tipo_transazione.segno END) AS n_entrata_conto_business,
+    COUNT(CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Famiglie' AND tipo_transazione.segno = '+' THEN tipo_transazione.segno END) AS n_entrata_conto_famiglie,
+
+    -- Importo transato in uscita per tipologia di conto
     SUM(CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Privati' AND tipo_transazione.segno = '-' THEN transazioni.importo ELSE 0 END) AS tot_uscita_conto_privati,
     SUM(CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Base' AND tipo_transazione.segno = '-' THEN transazioni.importo ELSE 0 END) AS tot_uscita_conto_base,
     SUM(CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Business' AND tipo_transazione.segno = '-' THEN transazioni.importo ELSE 0 END) AS tot_uscita_conto_business,
     SUM(CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Famiglie' AND tipo_transazione.segno = '-' THEN transazioni.importo ELSE 0 END) AS tot_uscita_conto_famiglie,
-    -- numero di conti per tipo di conto con operazioni in entrata
+    -- Importo transato in entrata per tipologia di conto
     SUM(CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Privati' AND tipo_transazione.segno = '+' THEN transazioni.importo ELSE 0 END) AS tot_entrata_conto_privati,
     SUM(CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Base' AND tipo_transazione.segno = '+' THEN transazioni.importo ELSE 0 END) AS tot_entrata_conto_base,
     SUM(CASE WHEN tipo_conto.desc_tipo_conto = 'Conto Business' AND tipo_transazione.segno = '+' THEN transazioni.importo ELSE 0 END) AS tot_entrata_conto_business,
@@ -41,4 +48,5 @@ CREATE TEMPORARY TABLE IF NOT EXISTS features (
   -- raggruppamento per id_cliente
   GROUP BY cliente.id_cliente
   -- assicuro che sia in ordine crescente per id_cliente
-  ORDER BY cliente.id_cliente);
+  ORDER BY cliente.id_cliente
+  );
